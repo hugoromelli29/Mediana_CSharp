3c94d7061e9689cac4995b4e5683e28bfd8eaba1
hugoromelli29 <hugoromelli@hotmail.com>
2022-07-28 17:03:04 -0300

    Upload files

---
+++ Mediana/Program.cs
@@ -0,0 +1,40 @@
+﻿Begin: Console.WriteLine("Deseja calcular a mediana de uma amostra de quantos números?");
+int qtd = int.Parse(Console.ReadLine());
+
+Array amostra = Array.CreateInstance(typeof(double), qtd);
+
+for (int i = 0; i < amostra.Length; i++)
+{
+    Console.Write($"Digite o {i + 1}º número: ");
+    amostra.SetValue(double.Parse(Console.ReadLine()), i);
+}
+
+TestaMediana(amostra);
+
+void TestaMediana(Array array)
+{
+    if ((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array está vazio");
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    double mediana = 0;
+    if (tamanho % 2 != 0)
+    {
+        mediana += numerosOrdenados[meio];
+    }
+    else if (tamanho % 2 == 0)
+    {
+        mediana += (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+    }
+    Console.WriteLine($"Mediana = {mediana}");
+}
+
+Console.ReadKey();
+Console.Clear();
+goto Begin;
